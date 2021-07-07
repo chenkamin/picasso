fd37eec6237e94f1cbb8edc614daf1dcf849f0c4
chenkamin <chenkamin@gmail.com>
2021-07-07 16:57:01 +0300

    bucket creation and upload route testing

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 
-app.get("/health", (req, res) => {
-    res.status(200).json({
-        message: "hello world"
-    })
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+        callback(null, '')
+    }
 })
+
+const upload = multer({ storage }).single('image')
+app.post('/upload', upload, (req, res) => {
+    console.log(req.file)
+})
+app.
+
+    app.get("/health", (req, res) => {
+        res.status(200).json({
+            message: "hello world"
+        })
+    })
 
 module.exports = app;
---
+++ awsService.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const ID = process.env.ACCESS_KEY_ID;
+const SECRET = process.env.SECRET_ACCESS_KEY;
+console.log(ID)
+const BUCKET_NAME = 'test-bucket-chen';
+
+
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+
+const params = {
+    Bucket: process.env.BUCKET_NAME,
+    CreateBucketConfiguration: {
+        LocationConstraint: "eu-west-1"
+    }
+};
+
+s3.createBucket(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log('Bucket Created Successfully', data.Location);
+});
+
+module.exports = awsService;
