4a73ca82905fb18446fe8061c20178a7ed05d5e7
chenkamin <chenkamin@gmail.com>
2021-07-09 15:22:26 +0300

    sharp tests

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const uuid = require('uuid')
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+const sharp = require('sharp')
 
 
 const storage = multer.memoryStorage({
@@ -16,40 +17,46 @@ const storage = multer.memoryStorage({
 
 const upload = multer({ storage }).single('image', 1)
 app.post('/upload', upload, async (req, res) => {
-    console.log(req.file)
+    // console.log(req.file)
     let myFile = req.file.originalname.split(".")
     const fileType = myFile[myFile.length - 1]
-
-    // console.log(req.file)
+    // const imageSizes = [{ sizeName: 'large', sizeInt: 2048 }]
+    // medium: 1024,
+    // thumb: 300
+    // }
+    // const scaleByHalf = await sharp(myFile)
+    //     .metadata()
+    //     .then(() => sharp(myFile)
+    //         .resize({
+    //             width: 200,
+    //             height: 200,
+    //         })
+    //         .toBuffer()
+    //     );
+    // console.log(scaleByHalf);
+    // const random = uuid();
     const params = [
         {
             Bucket: process.env.BUCKET_NAME,
             Key: `${uuid()}.${fileType}`,
             Body: req.file.buffer
-        },
-        {
-            Bucket: process.env.BUCKET_NAME,
-            Key: `${uuid()}111.${fileType}`,
-            Body: req.file.buffer
-        },
-        {
-            Bucket: process.env.BUCKET_NAME,
-            Key: `${uuid()}222.${fileType}`,
-            Body: req.file.buffer
         }
     ]
 
-    const responses = await Promise.all(
-        params.map(param => s3Bucket.s3.upload(param).promise())
-    )
+    // const responses = await Promise.all(
+    //     params.map(param => {
+    //         s3Bucket.s3.upload(param).promise()
+    //     })
 
-    res.status(200).send(responses)
-    // s3Bucket.s3.upload(params, (error, data) => {
-    //     if (error) {
-    //         res.status(500).send(error)
-    //     }
-    //     res.status(200).send(data)
-    // })
+    // )
+
+    // res.status(200).send(responses)
+    s3Bucket.s3.upload(params, (error, data) => {
+        if (error) {
+            res.status(500).send(error)
+        }
+        res.status(200).send(data)
+    })
 })
 
 app.get("/health", (req, res) => {
