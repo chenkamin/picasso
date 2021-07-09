dd1fe47229323156f38eeac057da2bca65e2528b
chenkamin <chenkamin@gmail.com>
2021-07-09 17:23:32 +0300

    routes dir and morgan loger

--- app.js
+++ app.js
@@ -1,73 +1,12 @@
-const AWS = require('aws-sdk');
-const s3Bucket = require('./awsService')
 const express = require('express');
-const multer = require('multer');
+const filesRouter = require('./routes/filesRouter')
 const app = express();
-const uuid = require('uuid')
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const sharp = require('sharp')
+const morgan = require('morgan')
+app.use(morgan('dev'));
 
 
-const storage = multer.memoryStorage({
-    destination: function (req, file, callback) {
-        callback(null, '')
-    }
-})
 
-const upload = multer({ storage }).single('image', 1)
-app.post('/upload', upload, async (req, res) => {
-    if (!req.file) {
-        res.status(404).json({
-            message: "please add file to response"
-        })
-        return
-    }
-    images = await resizeImages(req.file)
-    const responses = await Promise.all(
-        images.map(param => s3Bucket.s3.upload(param).promise())
-    )
+app.use('/api/v1/', filesRouter);
 
-
-    res.status(200).send(responses)
-})
-
-
-const resizeImages = async (image) => {
-    let myFile = image.originalname.split(".")
-    const fileType = myFile[myFile.length - 1]
-    const imageSizes = [
-        { sizeName: 'large', sizeInt: 2048 },
-        { sizeName: 'medium', sizeInt: 1024 },
-        { sizeName: 'thumb', sizeInt: 300 },
-    ]
-    const params = [];
-    for (s of imageSizes) {
-
-        const scaleByHalf = await sharp(image.buffer)
-            .metadata()
-            .then(() => sharp(image.buffer)
-                .resize({
-                    width: s['sizeInt'],
-                    height: s['sizeInt'],
-                })
-                .toBuffer()
-            );
-        console.log(scaleByHalf);
-        params.push({
-            Bucket: process.env.BUCKET_NAME,
-            name: s['sizeName'],
-            Key: `${uuid()}.${s['sizeName']}.${fileType}`,
-            Body: scaleByHalf
-        })
-    }
-    return params;
-
-}
-app.get("/health", (req, res) => {
-    res.status(200).json({
-        message: "hello world"
-    })
-})
 
 module.exports = app;
---
+++ routes/filesRouter.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+const sharp = require('sharp')
+const uuid = require('uuid')
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const s3Bucket = require('./../awsService')
+
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+        callback(null, '')
+    }
+})
+const upload = multer({ storage }).single('image', 1)
+
+router.post('/upload', upload, async (req, res) => {
+    if (!req.file) {
+        res.status(404).json({
+            message: "please add file to response"
+        })
+        return
+    }
+    images = await resizeImages(req.file)
+    const responses = await Promise.all(
+        images.map(param => s3Bucket.s3.upload(param).promise())
+    )
+
+
+    res.status(200).send(responses)
+})
+
+
+const resizeImages = async (image) => {
+    let myFile = image.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
+    const imageSizes = [
+        { sizeName: 'large', sizeInt: 2048 },
+        { sizeName: 'medium', sizeInt: 1024 },
+        { sizeName: 'thumb', sizeInt: 300 },
+    ]
+    const params = [];
+    for (s of imageSizes) {
+
+        const scaleByHalf = await sharp(image.buffer)
+            .metadata()
+            .then(() => sharp(image.buffer)
+                .resize({
+                    width: s['sizeInt'],
+                    height: s['sizeInt'],
+                })
+                .toBuffer()
+            );
+        console.log(scaleByHalf);
+        params.push({
+            Bucket: process.env.BUCKET_NAME,
+            name: s['sizeName'],
+            Key: `${uuid()}.${s['sizeName']}.${fileType}`,
+            Body: scaleByHalf
+        })
+    }
+    return params;
+
+}
+
+
+module.exports = router;
