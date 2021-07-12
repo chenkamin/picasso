6b9821e7ca8fe2d9e62b810b9da5f02f89bbb8c1
chenkamin <chenkamin@gmail.com>
2021-07-12 17:51:13 +0300

    authRemove env variable

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+        expiresIn: '90d'
     });
 };
 
@@ -15,7 +15,7 @@ const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
         expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            Date.now() + 90 * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     };
